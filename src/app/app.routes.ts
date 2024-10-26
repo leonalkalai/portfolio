c29648579c6c2f4af528b08d33f1fa73987d4fd5
leonalkalai <koundouras@gmail.com>
2024-10-26 21:05:56 +0000

    s

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
+// app.routes.ts
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './home/home.component'; // Adjust the path
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'contact', component: ContactComponent },
-];
+    { path: 'about', component: AboutComponent },
+      { path: 'contact', component: ContactComponent },
+      ];
