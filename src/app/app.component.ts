64128bbce49936e25a9cdfef996ed9064be969e7
leonalkalai <koundouras@gmail.com>
2024-10-27 10:26:37 +0000

    s

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component'; // Adjust paths as needed
+import { MainComponent } from './main/main.component'; // Adjust paths as needed
 import { FooterComponent } from './footer/footer.component';
 
 @Component({
@@ -8,6 +9,6 @@ import { FooterComponent } from './footer/footer.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent]  // Import RouterOutlet and custom components
+  imports: [RouterOutlet, HeaderComponent,  MainComponent, FooterComponent]  // Import RouterOutlet and custom components
 })
 export class AppComponent {}
