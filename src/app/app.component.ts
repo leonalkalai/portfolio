35ad6e510863ccb963ff3fa9a42ca7a1402f25bd
leonalkalai <koundouras@gmail.com>
2024-10-27 09:54:57 +0000

    s

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
-import { MainComponent } from './main/main.component';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component'; // Adjust paths as needed
 import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [HeaderComponent, MainComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, FooterComponent]  // Import RouterOutlet and custom components
 })
-export class AppComponent {
-  title = 'landing-page';
-}
+export class AppComponent {}
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,12 @@
-// app.config.ts
-import { ApplicationConfig, provideZoneChangeDetection, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-      provideZoneChangeDetection({ eventCoalescing: true }),
-          provideRouter(routes),
-            ],
-              schemas: [CUSTOM_ELEMENTS_SCHEMA] // Add this line
-              };
+    provideZoneChangeDetection({ eventCoalescing: true }), // Enables efficient change detection
+    provideRouter(routes), // Provides routing using routes defined in app.routes.ts
+    provideClientHydration() // Allows rehydration in Universal (SSR) apps
+  ]
+};
